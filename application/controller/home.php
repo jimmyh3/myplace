79af7411be99fd4c0a7366daad5628edd84dd9c9
Jimmy <jimmyh3@mail.sfsu.edu>
2016-11-24 18:38:15 -0800

    controller/home.php - changed search() logic to accommodate arrays.
    This is a basic step toward implementing min/max search function between two values. I uncommented HTML <input> with the names "min_price" and "max_price" in view/home/index.php and renamed them "actual_price[]" to target the "actual_price" column. The revised search() in home.php will handle the "[]" brackets.

--- application/controller/home.php
+++ application/controller/home.php
@@ -30,22 +30,23 @@ class Home extends Controller
             $query = $_POST['query'];
         if( isset($_POST['filters']))
             $filters = $_POST['filters'];
-        
+       
         $query_array = explode(" ", $query);
         
-        $apartments;
-        if( $filters == '') {
-            $apartments = $this->model->search( $query_array, array()); 
-        } else {
-            $result = array();
-            $filters = explode('&', $filters);
-            foreach( $filters as $filter) {
-                $key_value = explode( '=', $filter);
-                if( $key_value[ 1] !== '') $result[ $key_value[ 0]] = $key_value[ 1];
+        $filters_array = array();
+        /* Converts any "%##" (unsafe characters) to its actual value */
+        /* Creates an array of key value pairs based on a URL argument string */
+        parse_str(rawurldecode($filters), $filters_array);
+        
+        foreach (array_keys($filters_array) as $value)
+        {
+            /* Remove empty value elements in array */
+            if ($filters_array[$value] === ''){
+                unset($filters_array[$value]);
             }
-            
-            $apartments = $this->model->search( $query_array, $result);
         }
+        
+        $apartments = $this->model->search( $query_array, $filters_array);
         
         $results = "";
         if( !$apartments) {
@@ -55,6 +56,33 @@ class Home extends Controller
         }
 
         echo $results;
+        
+//        ---------------PREVIOUS VERSION-------------------
+//        
+//        $query_array = explode(" ", $query);
+//        
+//        $apartments;
+//        if( $filters == '') {
+//            $apartments = $this->model->search( $query_array, array()); 
+//        } else {
+//            $result = array();
+//            $filters = explode('&', $filters);
+//            foreach( $filters as $filter) {
+//                $key_value = explode( '=', $filter);
+//                if( $key_value[ 1] !== '') $result[ $key_value[ 0]] = $key_value[ 1];
+//            }
+//            
+//            $apartments = $this->model->search( $query_array, $result);
+//        }
+//        
+//        $results = "";
+//        if( !$apartments) {
+//            $results = "No Results!";
+//        } else {
+//            $results .=  $this->displayApartments( $apartments);   
+//        }
+//
+//        echo $results;
         
     }
       
