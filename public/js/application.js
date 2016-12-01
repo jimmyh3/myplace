597130f297411198e02dd6622e56dc7923dfc7e1
Jimmy <jimmyh3@mail.sfsu.edu>
2016-12-01 10:31:55 -0800

    public/js/application.js - moved duplicate ajax code into a function.

--- public/js/application.js
+++ public/js/application.js
@@ -1,28 +1,26 @@
 $(function() {
     
+    function submitSearch(e){
+        e.preventDefault();
+        
+        $.post(url + '/home/getFilters', {"filters": $('#ajax_filter_form').serialize()})
+            .done( function( filters) {
+               $.post(url + "/home/search", {"query" : $('#search_bar').val(), "filters" : filters})
+                    .done( function ( result) {
+                        $('#results').html( result);
+                    });
+            });
+    };
+    
     if($('#ajax_search').length !== 0) {
         $('#ajax_search').on('click', function( e){
-            e.preventDefault();
-            $.post(url + '/home/getFilters', {"filters": $('#ajax_filter_form').serialize()})
-                .done( function( filters) {
-                   $.post(url + "/home/search", {"query" : $('#search_bar').val(), "filters" : filters})
-                   .done( function ( result) {
-                       $('#results').html( result);
-                   });
-                });
+            search(e);
         });
     }
     
     if($('#ajax_filter_form') !== 0) { 
         $('#ajax_filter_form').on('submit', function( e){
-            e.preventDefault();
-            $.post(url + '/home/getFilters', {"filters": $('#ajax_filter_form').serialize()})
-                .done( function( filters) {
-                   $.post(url + "/home/search", {"query" : $('#search_bar').val(), "filters" : filters})
-                   .done( function ( result) {
-                       $('#results').html( result);
-                   });
-                });
+            search(e);
         });
     }
 });
