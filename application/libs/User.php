7cd2d8f99a533643e5b8b3980fc45e0261407894
Jimmy <jimmyh3@mail.sfsu.edu>
2016-11-30 20:17:56 -0800

    UserDB.php - Implemented addUser() and hasUser().
    NOTE: hasUser() uses fetch(), not fetchAll().
    Implemented a simple User class for storing values.

--- application/libs/User.php
+++ application/libs/User.php
@@ -7,10 +7,56 @@
  */
 class User {
     
-    private $userID     = 0;
+    private $userID     = NULL;
     private $email      = "";
     private $name       = "";
+    private $password   = "";
     private $userType   = 0;
+    
+    /**
+     * Set the user's ID.
+     * @param int $id - value to set user ID to.
+     */
+    public function setID($id)
+    {
+        $this->userID = $id;
+    }
+    
+    /**
+     * Set the user's email;
+     * @param String $email - value to set the user's email to.
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+    
+    /**
+     * Set the user's name.
+     * @param String $name - name of this user.
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    
+    /**
+     * Set the user's password.
+     * @param String $password - password of this user's account.
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+    
+    /**
+     * Set the type of this user.
+     * @param int $userType - 0 for student, 1 for landlords.
+     */
+    public function setType($userType)
+    {
+        $this->userType = $userType;
+    }
     
     /**
      * Get the unique user ID.
@@ -37,6 +83,11 @@ class User {
     public function getName()
     {
         return $this->name;
+    }
+    
+    public function getPassword()
+    {
+        return $this->password;
     }
     
     /**
--- application/model/UserDB.php
+++ application/model/UserDB.php
@@ -3,7 +3,7 @@
 require APP . 'libs/User.php';
 
 /**
- * Description of UserDB
+ * UserDB is responsible for handling User
  *
  * @author Jimmy
  */
@@ -21,13 +21,67 @@ class UserDB {
         }
     }
     
+    /**
+     * Add the given new User into the User database table.
+     * 
+     * @throws Exception - if the INSERT query fails.
+     * @param User $user - the user account to add.
+     * @return boolean $isAdded - true means successfully added, false if not.
+     */
     public function addUser(User $user)
     {
+        $isAdded = false;    //return this boolean
+        
+        $sql    = " INSERT INTO User
+                    (  name,  email,  password,  usertype)
+                    VALUES
+                    ( :name, :email, :password, :usertype) ";
+        
+        
+        if (! $this->hasUser($user->getEmail()) )
+        {
+            /* Prepare and Execute INSERT statement for adding a new User */
+            $stmt   = $this->db->prepare($sql);
+            $result = $stmt->execute(array( "name" => $user->getName(),
+                                            "email" => $user->getEmail(),
+                                            "password" => $user->getPassword(),
+                                            "usertype" => $user->getType()));
+            if ($result === false)
+                {throw new Exception('UserDB->addUser() - Could not execute INSERT User query'); }
+                
+            $isAdded = true;
+        }
+            
+        return $isAdded;
         
     }
     
-    public function checkUser(User $user)
+    /**
+     * Check if the given email belongs to any existing user account.
+     * 
+     * @param String $email - the user email to check for uniqueness.
+     * @return mixed - Returns the User record if the email exists, else false.
+     */
+    public function hasUser($email)
     {
+        $sql = " SELECT * FROM User 
+                 WHERE  email = :email ";
+        
+        
+        /* Prepare and Execute SELECT statement for email verification */
+        $stmt    = $this->db->prepare($sql); 
+        $result  = $stmt->execute(array( 
+                                    "email" => $email
+                                ));
+        if ($result === false)
+            {throw new Exception('UserDB->hasUser - Could not execute SELECT User query'); }
+                
+        /* should return 1 result or 0 due to SQL unique constraint */
+        //NOTE: this is using fetch() not fetchAll() [no foreach needed]
+        $account  = $stmt->fetch();
+        
+        return ($account) ? $account : false;
         
     }
+    
 }
