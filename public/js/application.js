f9cf935af939945f58c7cb603bec4e17c413ba53
Jimmy <jimmyh3@mail.sfsu.edu>
2016-12-01 23:21:19 -0800

    application.js - shifted out search functions into search.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,35 +1,5 @@
 $(function() {
     
-    /* Submit search arguments to controller/home.php */
-    function submitSearch(e){
-        e.preventDefault();
-        $.post(url + '/home/getFilters', {"filters": $('#ajax_filter_form').serialize()})
-            .done( function( filters) {
-               $.post(url + "/home/search", {"query" : $('#search_bar').val(), "filters" : filters})
-               .done( function ( result) {
-                   $('#results').html( result);
-               });
-            });
-    }
-    
-    if($('#ajax_search').length !== 0) {
-        $('#ajax_search').on('click', function( e){
-            submitSearch(e);
-        });
-        
-        $('#search_bar').keydown(function( e){
-            if (e.which === 13){    //13 === Enter Key
-                submitSearch(e);
-            }
-        });
-    }
-    
-    if($('#ajax_filter_form') !== 0) { 
-        $('#ajax_filter_form').on('submit', function( e){
-            submitSearch(e);
-        });
-    }
-    
     if($('#ajax_signin_form') !== 0) {
         $('#ajax_signin_form').on('submit', function( e) {
             e.preventDefault();
