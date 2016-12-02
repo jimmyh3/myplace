75b62e8dd749f4d54a1070b90004e5f4ccb961a0
Jimmy <jimmyh3@mail.sfsu.edu>
2016-12-01 19:01:55 -0800

    js/application.js - enabled "Enter Key" to submit for search bar.

--- public/js/application.js
+++ public/js/application.js
@@ -1,26 +1,32 @@
 $(function() {
     
+    /* Submit search arguments to controller/home.php */
     function submitSearch(e){
         e.preventDefault();
-        
         $.post(url + '/home/getFilters', {"filters": $('#ajax_filter_form').serialize()})
             .done( function( filters) {
                $.post(url + "/home/search", {"query" : $('#search_bar').val(), "filters" : filters})
-                    .done( function ( result) {
-                        $('#results').html( result);
-                    });
+               .done( function ( result) {
+                   $('#results').html( result);
+               });
             });
-    };
+    }
     
     if($('#ajax_search').length !== 0) {
         $('#ajax_search').on('click', function( e){
-            search(e);
+            submitSearch(e);
+        });
+        
+        $('#search_bar').keydown(function( e){
+            if (e.which === 13){    //13 === Enter Key
+                submitSearch(e);
+            }
         });
     }
     
     if($('#ajax_filter_form') !== 0) { 
         $('#ajax_filter_form').on('submit', function( e){
-            search(e);
+            submitSearch(e);
         });
     }
 });
