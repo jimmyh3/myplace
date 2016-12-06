957db8c62cde71f4c3740442e4d9d3f7a861906e
patbak15 <patbak15@mail.sfsu.edu>
2016-12-06 01:59:25 +0000

    adding structure to displayLandlordApts

--- application/controller/post.php
+++ application/controller/post.php
@@ -16,8 +16,18 @@ class Post extends Controller
         require APP . 'view/post/mypost.php';
         require APP . 'view/_templates/footer.php';
     }
-    
-   
+    public function displyLandlordApartments($user_id){
+        // get from DB landlord apartments 
+        $apartments = $this->model->getLandLordApartments($user_id); 
+        if(!$apartments){
+            //display No Apartments
+            echo "No Apartments Listed"; 
+        }else{
+            foreach($apartments as $apartment){
+                // enter loop format here 
+            }
+        }
+    }
     
 }
 
