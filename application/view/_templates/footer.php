df72cd271b2bc6de52513d3fb44c9c6f1d8a47f0
Ulises <ulisesm@mail.sfsu.edu>
2016-12-12 03:17:31 +0000

    no fixed filter for mobile devices

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -55,51 +55,13 @@
 
 <script src="<?php echo URL; ?>js/myplace-apartment_listing.js"></script>
 
-<!--<script>
-    var stickySidebar = $('.sticky');
-
-if (stickySidebar.length > 0) { 
-  var stickyHeight = stickySidebar.height(),
-      sidebarTop = stickySidebar.offset().top;
-}
-
-// on scroll move the sidebar
-$(window).scroll(function () {
-  if (stickySidebar.length > 0) { 
-    var scrollTop = $(window).scrollTop();
-            
-    if (sidebarTop < scrollTop) {
-      stickySidebar.css('top', scrollTop - sidebarTop);
-
-      // stop the sticky sidebar at the footer to avoid overlapping
-      var sidebarBottom = stickySidebar.offset().top + stickyHeight,
-          stickyStop = $('.mainPage').offset().top + $('.mainPage').height();
-      if (stickyStop < sidebarBottom) {
-        var stopPosition = $('.mainPage').height() - stickyHeight;
-        stickySidebar.css('top', stopPosition);
-      }
-    }
-    else {
-      stickySidebar.css('top', '0');
-    } 
-  }
-});
-
-$(window).resize(function () {
-  if (stickySidebar.length > 0) { 
-    stickyHeight = stickySidebar.height();
-  }
-});
-</script>-->
 
 <script>
-    //Side menu floater
-$('#sidebar').affix({
-      offset: {
+    // affixed filter
+    $('#sidebar').affix({
+        offset: {
         top: $('#headerwrap').height() + 50,
-        //top:350,
         bottom: $('#footer').outerHeight(true) + 60
-
       }
 });
 </script>
